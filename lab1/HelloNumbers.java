c01f2ee05214f2230b2372d7f3691d26d132e086
spdf_fyer <fuy60703@gmail.com>
2024-02-14 22:18:24 +0800

    completed lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers{
+    public static void main(String[] args){
+        int x=0;
+        int i=1;
+        while(x<=45){
+            System.out.print(x+" ");
+            x+=i;
+            i+=1;
+        }
+    }
+}
--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,14 @@ public class LeapYear {
             }
         }
     }
+
+    public static boolean isLeapYear(int year){
+        if(year%400==0||(year%4==0&&year%100!=0)){
+            return true;
+        }
+        else{
+            return false;
+        }   
+    }
 }
 
