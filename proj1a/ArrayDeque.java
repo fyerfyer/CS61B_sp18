55ff7740f951bddc96ce827462491b101fad0b12
spdf_fyer <fuy60703@gmail.com>
2024-02-19 20:23:53 +0800

    completed proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,137 +1,135 @@
 public class ArrayDeque<T> {
+
     private T[] items;
-    private int prevpos;
-    private int lastpos;
     private int size;
+
     private int length;
+
+    private int prev;
+
+    private int last;
 
     public ArrayDeque() {
         items = (T[]) new Object[8];
-        prevpos = 0;
-        lastpos = 0;
         size = 0;
         length = 8;
-    }
-
-    private int stepForward(int index, int module) {
-        index %= module;
-        if(index == module - 1) {
-            return 0;
-        }
-        return index + 1;
-    }
-
-    private int stepBackward(int index) {
-        if(index == 0) {
-            return length - 1;
-        }
-        return index - 1;
-    }
-
-    private void resizeIfNecessary() {
-        if (size == length - 1) {
-            Resizea();
-        } else if (length >= 16 && length / size >= 4) {
-            Resizeb();
-        }
-    }
-
-    private void Resizea() {
-        T[] newArray = (T[]) new Object[length * 2];
-        int ptr1 = prevpos;
-        int ptr2 = length;
-        while (ptr1 != lastpos) {
-            newArray[ptr2] = items[ptr1];
-            ptr1 = stepForward(ptr1, length);
-            ptr2 = stepForward(ptr2, length * 2);
-        }
-        prevpos = length;
-        lastpos = ptr2;
-        items = newArray;
-        length *= 2;
-    }
-
-    private void Resizeb() {
-        T[] newArray = (T[]) new Object[length / 2];
-        int ptr1 = prevpos;
-        int ptr2 = length / 4;
-        while (ptr1 != lastpos) {
-            newArray[ptr2] = items[ptr1];
-            ptr1 = stepForward(ptr1, length);
-            ptr2 = stepForward(ptr2, length / 2);
-        }
-        prevpos = length / 4;
-        lastpos = ptr2;
-        items = newArray;
-        length /= 2;
-    }
-
-    public void addFirst(T item) {
-        resizeIfNecessary();
-        prevpos = stepBackward(prevpos);
-        items[prevpos] = item;
-        size += 1;
-//        resizeIfNecessary();
-    }
-
-    public void addLast(T item) {
-        resizeIfNecessary();
-        items[lastpos] = item;
-        size += 1;
-        lastpos = stepForward(lastpos, length);
-//        resizeIfNecessary();
-    }
-
-    public T removeFirst() {
-        if (isEmpty()) {
-            return null;
-        }
-
-        T removeElement = items[prevpos];
-        prevpos = stepForward(prevpos, length);
-        items[prevpos] = null;
-        size -= 1;
-        resizeIfNecessary();
-        return removeElement;
-    }
-
-    public T removeLast() {
-        if (isEmpty()) {
-            return null;
-        }
-
-        lastpos = stepBackward(lastpos);
-        size -= 1;
-        T removeElement = items[lastpos];
-        resizeIfNecessary();
-        return removeElement;
-    }
-
-    public int size() {
-        return size;
+        prev = 4;
+        last = 4;
     }
 
     public boolean isEmpty() {
         return size == 0;
     }
 
-    public void printDeque() {
-        int current = prevpos;
-        for (int i = 0; i < size; i += 1) {
-            System.out.print(items[current] + " ");
-            current = stepForward(current, length);
+    public int size() {
+        return size;
+    }
+
+    private int stepForward(int index) {
+        if (index == 0) {
+            return length - 1;
         }
+        return index - 1;
+    }
+
+    private int stepBackward(int index, int module) {
+        index %= module;
+        if (index == module - 1) {
+            return 0;
+        }
+        return index + 1;
+    }
+
+    private void larger() {
+        T[] newArray = (T[]) new Object[length * 2];
+        int ptr1 = prev;
+        int ptr2 = length;
+        while (ptr1 != last) {
+            newArray[ptr2] = items[ptr1];
+            ptr1 = stepBackward(ptr1, length);
+            ptr2 = stepBackward(ptr2, length * 2);
+        }
+        prev = length;
+        last = ptr2;
+        items = newArray;
+        length *= 2;
+    }
+
+    private void smaller() {
+        T[] newArray = (T[]) new Object[length / 2];
+        int ptr1 = prev;
+        int ptr2 = length / 4;
+        while (ptr1 != last) {
+            newArray[ptr2] = items[ptr1];
+            ptr1 = stepBackward(ptr1, length);
+            ptr2 = stepBackward(ptr2, length / 2);
+        }
+        prev = length / 4;
+        last = ptr2;
+        items = newArray;
+        length /= 2;
+    }
+
+    public void addFirst(T item) {
+        if (size == length - 1) {
+            larger();
+        }
+        prev = stepForward(prev);
+        items[prev] = item;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        if (size == length - 1) {
+            larger();
+        }
+        items[last] = item;
+        last = stepBackward(last, length);
+        size += 1;
+    }
+
+    public T removeFirst() {
+        if (length >= 16 && length / size >= 4) {
+            smaller();
+        }
+        if (size == 0) {
+            return null;
+        }
+        T removeElement = items[prev];
+        prev = stepBackward(prev, length);
+        size -= 1;
+        return removeElement;
+    }
+
+    public T removeLast() {
+        if (length >= 16 && length / size >= 4) {
+            smaller();
+        }
+        if (size == 0) {
+            return null;
+        }
+        last = stepForward(last);
+        size -= 1;
+        return items[last];
     }
 
     public T get(int index) {
-        if (index < 0 || index >= size) {
+        if (index >= size) {
             return null;
         }
-
-        int prev = prevpos;
-        for (int i = 0; i < index; i += 1) {
-            prev = stepForward(prev, length);
+        int ptr = prev;
+        for (int i = 0; i < index; i++) {
+            ptr = stepBackward(ptr, length);
         }
-        return items[prev];
+        return items[ptr];
     }
+
+    public void printDeque() {
+        int ptr = prev;
+        while (ptr != last) {
+            System.out.print(items[ptr] + " ");
+            ptr = stepBackward(ptr, length);
+        }
+    }
+    
 }
