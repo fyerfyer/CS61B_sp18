d28e7017d0858dba5d2d3f9eb1e2c7007f948c51
spdf_fyer <fuy60703@gmail.com>
2024-02-21 15:29:33 +0800

    completed proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class TestArrayDequeGold {
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    @Test
+    public void testRandomCalls() {
+        int time = 100;
+        StudentArrayDeque<Integer> stu = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ans = new ArrayDequeSolution<>();
+        String error = "";
+        while (time > 0) {
+            int operation = StdRandom.uniform(4);
+            if (operation == 0) {
+                int addNum = StdRandom.uniform(1000);
+                stu.addFirst(addNum);
+                ans.addFirst(addNum);
+                error = error + "addFirst(" + addNum + ")\n";
+            } else if (operation == 1) {
+                int addNum = StdRandom.uniform(1000);
+                stu.addLast(addNum);
+                ans.addLast(addNum);
+                error = error + "addFirst(" + addNum + ")\n";
+            } else if (operation == 2) {
+                if (!stu.isEmpty()) {
+                    int removeStu = stu.removeFirst();
+                    int removeAns = ans.removeFirst();
+                    error = error + "removeFirst()\n";
+                    assertEquals(error, removeStu, removeAns);
+                }
+            } else {
+                if (!stu.isEmpty()) {
+                    int removeStu = stu.removeLast();
+                    int removeAns = ans.removeLast();
+                    error = error + "removeLast()\n";
+                    assertEquals(error, removeStu, removeAns);
+                }
+            }
+            time -= 1;
+        }
+    }
 }
