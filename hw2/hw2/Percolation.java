835e4660963cbdc2bc286a82252576ebcacd5716
spdf_fyer <fuy60703@gmail.com>
2024-02-29 18:25:20 +0800

    completed hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -7,12 +7,16 @@ public class Percolation {
     private int[][] arr;
     private WeightedQuickUnionUF site;
     private int size;
+    private int bottomConnector;
+    private int upperConnector;
 
     public Percolation(int N) {
         if (N <= 0) {
             throw new java.lang.IllegalArgumentException("Invalid index!");
         }
-        site = new WeightedQuickUnionUF(N * N + 1);
+        bottomConnector = N * N;
+        upperConnector = N * N + 1;
+        site = new WeightedQuickUnionUF(N * N + 2);
         size = 0;
         arr = new int[N][N];
         for (int i = 0; i < N; i += 1) {
@@ -20,56 +24,61 @@ public class Percolation {
                 arr[i][j] = 0;
             }
         }
+
+        for (int i = 0; i < N; i += 1) {
+            site.union(bottomConnector, i);
+            site.union(upperConnector, i + N * (N - 1));
+        }
+    }
+
+    private boolean validatePos(int row, int col) {
+        if (row < 0 || row > arr.length - 1 || col < 0 || col > arr.length - 1) {
+            return false;
+        }
+        return true;
+    }
+
+    private void connect(int prerow, int precol, int nowrow, int nowcol) {
+        if (!validatePos(nowrow, nowcol) || !validatePos(prerow, precol)) {
+            return;
+        }
+        if (arr[nowrow][nowcol] == 1) {
+            site.union(precol * arr.length + precol, nowrow * arr.length + nowcol);
+        }
     }
 
     // open the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (row < 0 || row > arr.length - 1 || col < 0 || col > arr.length - 1) {
+        if (!validatePos(row, col)) {
             throw new java.lang.IndexOutOfBoundsException("Invalid index!");
         }
-
         if (arr[row][col] == 0) {
             arr[row][col] = 1;
             size += 1;
-            int index = row * arr.length + col;
-            int[] positionIndex = new int[4];
-            // up
-            if (row == 0 || arr[row - 1][col] == 0) {
-                positionIndex[0] = -1;
-            }
-            positionIndex[0] = (row - 1) * arr.length + col;
-            //down
-            if (row == arr.length - 1 || arr[row + 1][col] == 0) {
-                positionIndex[1] = -1;
-            }
-            positionIndex[1] = (row + 1) * arr.length + col;
-            //left
-            if (col == 0 || arr[row][col - 1] == 0) {
-                positionIndex[2] = -1;
-            }
-            positionIndex[2] = row * arr.length + col - 1;
-            //right
-            if (col == arr.length - 1 || arr[row][col + 1] == 0) {
-                positionIndex[3] = -1;
-            }
-            positionIndex[3] = row * arr.length +col + 1;
-
-            for (int i = 0; i < 4; i += 1) {
-                if (positionIndex[i] >= 0) {
-                    site.union(positionIndex[i], index);
-                }
-            }
+            connect(row, col, row + 1, col);
+            connect(row, col, row - 1, col);
+            connect(row, col, row, col - 1);
+            connect(row, col, row, col + 1);
         }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
+        if (!validatePos(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException("Invalid index!");
+        }
         return arr[row][col] == 1;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return size == arr.length * arr.length;
+        if (!validatePos(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException("Invalid index!");
+        }
+        if (!isOpen(row, col)) {
+            return false;
+        }
+        return site.connected(bottomConnector, row * arr.length + col);
     }
 
     // number of open sites
@@ -79,15 +88,11 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        for (int i = 0; i < arr.length; i += 1) {
-            for (int j = 0; j < arr.length; j += 1) {
-                if (site.connected(i, j + arr.length * (arr.length - 1))) {
-                    return true;
-                }
-            }
-        }
+        return site.connected(upperConnector, bottomConnector);
+    }
 
-        return false;
+    // use for unit testing (not required)
+    public static void main(String[] args) {
     }
 
 }
